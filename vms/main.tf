0c9bb3fb32750641ccf32a4c13a5ceb149d2fc71
Tom Schinelli <tom.schinelli@gmail.com>
2023-05-23 19:49:13 +0200

    Add debian 11 support

--- vms/main.tf
+++ vms/main.tf
@@ -24,3 +24,17 @@ module "demo-2304" {
   ci_user      = "tom"
   ci_sshkeys   = file("~/.ssh/id_rsa.pub")
 }
+
+module "demo-debian-11" {
+  source        = "./modules/full_clone"
+  name          = "demo-debian-11"
+  description   = "Demo vm created via terraform"
+  vmid          = "103"
+  template_name = "debian-11"
+
+  disk_size    = "10G"
+  disk_storage = "fast1"
+  ci_ipconfig  = "ip=192.168.1.205/24,gw=192.168.1.1"
+  ci_user      = "tom"
+  ci_sshkeys   = file("~/.ssh/id_rsa.pub")
+}
