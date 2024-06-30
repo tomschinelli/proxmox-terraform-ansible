da790335cf2361e73555f7cb0ed6b62d89029fd6
Tom Schinelli <tom.schinelli@gmail.com>
2024-06-30 21:58:35 +0200

    Add bpg proxmox provider (dirty yet working example)

--- vms/main.tf
+++ vms/main.tf
@@ -1,40 +1,69 @@
-module "demo" {
-  source        = "./modules/full_clone"
-  name          = "demo"
-  description   = "Demo vm created via terraform"
-  vmid          = "101"
-  template_name = "ubuntu-server-2204"
+# module "demo-debian-11" {
+#   source        = "./modules/full_clone"
+#   name          = "demo-debian-11"
+#   description   = "Demo vm created via terraform"
+#   vmid          = "101"
+#   template_name = "debian-11"
+#   target_node   = "nuc01"
+#
+#   disk_size    = "10G"
+#   disk_storage = "local-lvm"
+#   ci_ipconfig  = "ip=192.168.8.222/24,gw=192.168.8.1"
+#   ci_user      = "tom"
+#   ci_sshkeys   = file("~/.ssh/id_rsa.pub")
+# }
 
-  disk_size    = "10G"
-  disk_storage = "fast1"
-  ci_ipconfig  = "ip=192.168.1.203/24,gw=192.168.1.1"
-  ci_user      = "tom"
-  ci_sshkeys   = file("~/.ssh/id_rsa.pub")
-}
-module "demo-2304" {
-  source        = "./modules/full_clone"
-  name          = "demo-2304"
-  description   = "Demo vm created via terraform"
-  vmid          = "102"
-  template_name = "ubuntu-server-2304"
-
-  disk_size    = "10G"
-  disk_storage = "fast1"
-  ci_ipconfig  = "ip=192.168.1.204/24,gw=192.168.1.1"
-  ci_user      = "tom"
-  ci_sshkeys   = file("~/.ssh/id_rsa.pub")
+resource "proxmox_virtual_environment_download_file" "debian_12" {
+  content_type        = "iso"
+  datastore_id        = "local"
+  file_name           = "debian-12-generic-amd64.img"
+  node_name           = "nuc01"
+  url                 = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2"
+  overwrite           = true
+  overwrite_unmanaged = true
 }
 
-module "demo-debian-11" {
-  source        = "./modules/full_clone"
-  name          = "demo-debian-11"
-  description   = "Demo vm created via terraform"
-  vmid          = "103"
-  template_name = "debian-11"
+data "local_file" "ssh_public_key" {
+  filename = pathexpand("~/.ssh/id_rsa.pub")
+}
 
-  disk_size    = "10G"
-  disk_storage = "fast1"
-  ci_ipconfig  = "ip=192.168.1.205/24,gw=192.168.1.1"
-  ci_user      = "tom"
-  ci_sshkeys   = file("~/.ssh/id_rsa.pub")
+resource "proxmox_virtual_environment_vm" "demo" {
+#   name      = "demo_debian_12"
+  description = "Managed by Terraform"
+  tags        = ["terraform", "ubuntu"]
+
+  node_name = "nuc01"
+  vm_id     = "102"
+
+#   keyboard_layout = ""
+  initialization {
+    user_account {
+      username = "tom"
+      keys = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.8.222/24"
+        gateway = "192.168.8.1"
+      }
+    }
+  }
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.debian_12.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 40
+  }
+  cpu {
+    cores = 4
+  }
+  memory {
+    dedicated = 4096
+  }
+  network_device {
+    bridge = "vmbr0"
+  }
 }
--- vms/provider.tf
+++ vms/provider.tf
@@ -1,28 +1,52 @@
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source  = "telmate/proxmox"
+#       version = ">= 2.9.14"
+#     }
+#   }
+# }
+# #
+# variable "proxmox_api_url" {
+#   type = string
+# }
+# variable "proxmox_api_token_id" {
+#   type      = string
+#   sensitive = true
+# }
+# variable "proxmox_api_token_secret" {
+#   type      = string
+#   sensitive = true
+# }
+# provider "proxmox" {
+#   pm_api_url          = var.proxmox_api_url
+#   pm_api_token_id     = var.proxmox_api_token_id
+#   pm_api_token_secret = var.proxmox_api_token_secret
+#
+#   pm_tls_insecure = true
+# }
+#
+
+
 terraform {
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"
-      version = ">= 2.9.14"
+      source  = "bpg/proxmox"
+      version = ">= 0.60.1"
     }
   }
 }
-#
-variable "proxmox_api_url" {
-  type = string
-}
-variable "proxmox_api_token_id" {
-  type      = string
-  sensitive = true
-}
-variable "proxmox_api_token_secret" {
-  type      = string
-  sensitive = true
-}
+
+variable "proxmox_endpoint" {}
+variable "proxmox_api_token" {}
+
 provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_api_token_id     = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
-
-  pm_tls_insecure = true
+  endpoint  = var.proxmox_endpoint
+  api_token = var.proxmox_api_token
+  insecure  = true
+  ssh {
+    agent    = false
+    username = "root"
+    private_key = file("~/.ssh/id_rsa")
+  }
 }
-
